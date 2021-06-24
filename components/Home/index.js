b0496ac7bf20eac79230ceee48a0ba15d55ee8cc
Sowmen Rahman <sowmen.appd@gmail.com>
2021-06-24 13:22:21 +0600

    Fix schema object

--- components/Home/index.js
+++ components/Home/index.js
@@ -8,28 +8,7 @@ const Home = () => (
       title="SRDev"
       description="Portfolio of Sowmen, a full stack web/app developer, game-dev mentor and ML/DL enthusiast"
     />
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{
-        __html: JSON.stringify({
-          "@context": "https://schema.org/",
-          "@type": "Person",
-          name: "Sowmen Rahman",
-          url: process.env.NEXT_PUBLIC_VERCEL_URL,
-          image: process.env.NEXT_PUBLIC_VERCEL_URL + "/images/dp.png",
-          sameAs: [
-            "https://www.facebook.com/sowmen.rahman.01",
-            "https://twitter.com/SowmenR",
-            "https://www.instagram.com/art1san__",
-            "https://www.youtube.com/channel/UC5vjE1_tiI-sOhcZTT0AI8A",
-            "https://www.linkedin.com/in/sowmen-rahman-01/",
-            "https://github.com/sowmenappd",
-            "https://sowmenrahman.tech",
-          ],
-          jobTitle: "Software Engineer",
-        }),
-      }}
-    />
+
     <Projects />
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,28 @@ const Index = () => (
     <Header />
     <Intro />
     <Home />
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          "@context": "https://schema.org/",
+          "@type": "Person",
+          name: "Sowmen Rahman",
+          url: process.env.NEXT_PUBLIC_VERCEL_URL,
+          image: process.env.NEXT_PUBLIC_VERCEL_URL + "/images/dp.png",
+          sameAs: [
+            "https://www.facebook.com/sowmen.rahman.01",
+            "https://twitter.com/SowmenR",
+            "https://www.instagram.com/art1san__",
+            "https://www.youtube.com/channel/UC5vjE1_tiI-sOhcZTT0AI8A",
+            "https://www.linkedin.com/in/sowmen-rahman-01/",
+            "https://github.com/sowmenappd",
+            "https://sowmenrahman.tech",
+          ],
+          jobTitle: "Software Engineer",
+        }),
+      }}
+    />
   </Layout>
 );
 
